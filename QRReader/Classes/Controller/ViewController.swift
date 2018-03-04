cdf5cfab13e712979548a8fa313a6c9b128a0cf9
Ammad <ammadakhtar@live.com>
2018-03-04 21:00:27 +0500

    First push

---
+++ QRReader/Classes/Controller/ViewController.swift
@@ -0,0 +1,72 @@
+// Created By Ammad Akhtar
+// ViewController.swift
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    // MARK: - IBOutlets and variables
+
+    @IBOutlet weak var square: UIImageView!
+    var video = AVCaptureVideoPreviewLayer()
+
+    // MARK: - UIViewController Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        startCaptureQRCode()
+    }
+
+    // MARK: - Private Methods
+
+    func startCaptureQRCode() {
+        //Creating session
+        let session = AVCaptureSession()
+
+        //Define capture devcie
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+
+        } catch {
+            print ("ERROR")
+        }
+
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+
+        self.view.bringSubview(toFront: square)
+
+        session.startRunning()
+    }
+
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        if metadataObjects != nil && metadataObjects.count != 0 {
+
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+
+                if object.type == AVMetadataObjectTypeQRCode {
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
